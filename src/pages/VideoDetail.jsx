b23b282e37ef5bcc54d0d960b6548e7646d9b423
mayrang <pkss0626@naver.com>
2023-03-23 17:40:33 +0900

    디테일 틀 구성

--- src/pages/VideoDetail.jsx
+++ src/pages/VideoDetail.jsx
@@ -1,5 +1,24 @@
 import React from "react";
+import { useParams } from "react-router-dom";
 
 export default function VideoDetail() {
-  return <div></div>;
+  const { videoId } = useParams();
+
+  return (
+    <div className="flex h-full w-full">
+      <div className="w-9/12 bg-white">
+        <iframe
+          src={`https://www.youtube.com/embed/${videoId}`}
+          className=""
+          type="text/html"
+          id="player"
+          width="560"
+          height="315"
+          title="player"
+          frameborder="0"
+        ></iframe>
+      </div>
+      <div className="w-3/12 bg-red-500"></div>
+    </div>
+  );
 }
