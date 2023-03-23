849b32679ee475f2b565b9365278c04a89cbff85
mayrang <pkss0626@naver.com>
2023-03-23 18:15:18 +0900

    ifrmae 수정(안됨)

--- src/pages/VideoDetail.jsx
+++ src/pages/VideoDetail.jsx
@@ -8,13 +8,12 @@ export default function VideoDetail() {
     <div className="flex h-full w-full">
       <div className="w-9/12 bg-white">
         <iframe
-          src={`https://www.youtube.com/embed/${videoId}`}
-          className=""
-          type="text/html"
           id="player"
-          width="560"
-          height="315"
-          title="player"
+          type="text/html"
+          title="test"
+          width="640"
+          height="360"
+          src={`http://www.youtube.com/embed/${videoId}?enablejsapi=1&origin=http://example.com`}
           frameborder="0"
         ></iframe>
       </div>
